dc44e44783ec77e30bbe6b3a29c9febb4ae319a3
Ye Rui <yerui@connect.hku.hk>
2018-08-16 10:41:52 +0800

    delete pcr error while zipHet

--- main.h
+++ main.h
@@ -444,8 +444,8 @@ vector< mCharDouble > zipHetPoint(const vector< mCharDouble > &w, const vector< 
             if ( ci != c[j].end() ) {
                 if ( ci->second > opt.pvalue ) continue;
 
-                nt[ wi->first ] = wi->second + ci->second - wi->second * ci->second
-                                + 10 * pow(opt.pcrError,2);
+                nt[ wi->first ] = wi->second + ci->second - wi->second * ci->second;
+//                                + 10 * pow(opt.pcrError,2);
             }
         }
 
